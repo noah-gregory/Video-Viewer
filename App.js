59aa5416123353934bf74f1763d8b09380845f53
noah-gregory <noah.gregory.fl@gmail.com>
2023-12-27 17:18:16 -0500

    Initial commit

--- App.js
+++ App.js
@@ -1,13 +1,28 @@
-import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  return(
+    <NavigationContainer theme={DarkTheme}>
+    <Stack.Navigator initialRouteName="IPConnect Screen"
+     screenOptions={{
+      headerShown: false,
+      cardStyle: { backgroundColor: 'transparent' }
+  }}>
+      <Stack.Screen 
+        name="Video Screen" 
+        component={VideoScreen}   
+        options={{ animation: 'none'}}
+      />
+      <Stack.Screen 
+        name="IPConnect Screen" 
+        component={IPConnectScreen}
+        options={{ animation: 'none'}}
+ 
+      />
+    </Stack.Navigator>
+  </NavigationContainer>
+);
 }
 
 const styles = StyleSheet.create({
---
+++ src/components/app/videoScreen.js
@@ -0,0 +1,35 @@
+import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { WebView } from 'react-native-webview';
+
+
+export default class VideoScreen extends React.Component {
+
+    constructor(props) {
+        super(props);
+    
+        this.state = {
+          ip: '',
+        };
+    
+        this.EZRASSOR = new EZRASSOR(this.state.ip);
+    }
+
+    render(){
+        return (
+            <WebView
+              style={styles.container}
+              source={{ uri: ip }}
+            />
+          );
+    }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
